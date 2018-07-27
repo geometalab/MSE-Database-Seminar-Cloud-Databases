3e22935ddeaf4757d9ca6b1e6bc411aead105417
Samuel Kurath <samuel.kurath@gmail.com>
2018-07-27 11:28:01 +0200

    working on script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CLOUD SPANNER
+# Database Seminar HSR, Fall 2018: CLOUD SPANNER
 ## Setup
 To use Google Cloud Spanner you need to have Google account.  
 After that you are able to activate the coupon of the DB Seminar.  
@@ -9,21 +9,27 @@ If everything works as expected you have 100$ for your intentions.
 ### gcloud
 To interact with the Google Cloud from your own device you need to install the gcloud tool.  
 The installation is described on: https://cloud.google.com/sdk/gcloud/  
-Don't forget to initialize gloud after the installation. The command is listed below.  
+Don't forget to initialize gloud and set the project id after the installation. The commands are listed below.  
 ```bash
 gcloud init
+gcloud config set project [MY_PROJECT_ID]
 ```
 
 If gcloud is initialised you are ready to set up a project as you see in:  
-https://cloud.google.com/spanner/docs/getting-started/set-up
+https://cloud.google.com/spanner/docs/getting-started/set-up  
 
 ### Python
 The first steps using Cloud Spanner in combination with Python are described under:  
-https://cloud.google.com/spanner/docs/getting-started/python/
+https://cloud.google.com/spanner/docs/getting-started/python/  
 
-The repository python-docs-samples provides examples for the basic usage.
-The main dependencies are:  
+The repository python-docs-samples provides examples for the basic usage.  
+The main dependencies are (as you can see in the requirements.txt):  
 ```
 google-cloud-spanner==1.3.0
 futures==3.2.0; python_version < "3"
 ```
+
+## Dashboard
+To get an overview of your Google Clouds Services have a look at:  
+* https://console.cloud.google.com/home/
+* https://console.cloud.google.com/apis
--- spanner.py
+++ spanner.py
@@ -1,27 +1,69 @@
+import os
+import argparse
+
 from google.cloud import spanner
 from subprocess import call
 
 database_name = 'york'
-instance_id = 'red-dominion-109811'
+instance_id = 'paris-instance'
+instance_config = 'regional-europe-west1'
+number_of_nodes = 1
+instance_description = 'db_seminar'
+project_id = 'red-dominion-109811'
+region = 'europe-west1'
+
+
+def set_credentials(path_to_json_file):
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = path_to_json_file
 
 
 def run():
-    command = 'gcloud sql databases create {}'.format(database_name).split()
-    call(command)
+    #    create_instance()
+    create_database()
 
+
+def create_instance():
+    create_instance_command = 'gcloud spanner instances create {0} --config={1} --description={2} --nodes={3}'.format(
+        instance_id, instance_config, instance_description, number_of_nodes)
+    call(create_instance_command)
+
+
+def create_database():
     spanner_client = spanner.Client()
     instance = spanner_client.instance(instance_id)
 
-    database = instance.database(database_name)
+    database = instance.database(database_name, ddl_statements=[
+        """
+            CREATE TABLE trips (
+              id INT64,
+              cab_type_id INT64,
+              passenger_count INT64,
+              pickup_datetime timestamp,
+              dropoff_datetime timestamp,
+              pickup_longitude FLOAT64,
+              pickup_latitude FLOAT64,
+              dropoff_longitude FLOAT64,
+              dropoff_latitude FLOAT64,
+              trip_distance FLOAT64,
+              fare_amount FLOAT64,
+              total_amount FLOAT64
+            )PRIMARY KEY (id);
+        """
+    ])
 
-    # Execute a simple SQL statement.
-    with database.snapshot() as snapshot:
-        results = snapshot.execute_sql('SELECT 1')
+    operation = database.create()
 
-        for row in results:
-            print(row)
-            # [END spanner_quickstart]
+    print('Waiting for operation to complete...')
+    operation.result()
+
+    print('Created database {} on instance {}'.format(
+        database_name, instance_id))
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Google Cloud Spanner script for the DB Seminar HSR, Fall 2018', )
+    parser.add_argument('-c', '--credentials', dest='credentials', help='Path to the JSON credential file')
+    parser.set_defaults(credentials='credentials.json')
+    args = parser.parse_args()
+    set_credentials(args.credentials)
     run()
